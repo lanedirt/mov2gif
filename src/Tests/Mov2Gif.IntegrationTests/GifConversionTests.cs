e246b6091329fe5582ca1c1a1c7c75b5a88c5a10
Leendert de Borst <ldeborst@xivisoft.com>
2024-11-24 15:22:56 +0100

    Add integration tests

---
+++ src/Tests/Mov2Gif.IntegrationTests/GifConversionTests.cs
@@ -0,0 +1,61 @@
+﻿using Mov2Gif.Utilities.GifConverter;
+
+namespace Mov2Gif.IntegrationTests;
+
+public class GifConversionTests
+{
+    private MovToGifConverter _converter;
+    private const string TestDataNamespace = "Mov2Gif.IntegrationTests.TestData";
+
+    [SetUp]
+    public void Setup()
+    {
+        _converter = new MovToGifConverter();
+    }
+
+
+    [Test]
+    public async Task Should_ProduceValidGif_When_ConvertingRickrollMov()
+    {
+        // Arrange
+        await using var movStream = typeof(GifConversionTests).Assembly
+            .GetManifestResourceStream($"{TestDataNamespace}.rickroll.mov");
+
+        Assert.That(movStream, Is.Not.Null, "Test MOV file should exist in embedded resources");
+
+        // Act
+        await using var gifStream = await _converter.ConvertToGif(movStream, frameRate: 15);
+
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.That(gifStream, Is.Not.Null);
+            Assert.That(gifStream.Length, Is.GreaterThan(0), "GIF file size should be greater than 0");
+        });
+    }
+
+    [Test]
+    public async Task Should_ProduceValidGif_When_UsingCustomOptions()
+    {
+        // Arrange
+        await using var movStream = typeof(GifConversionTests).Assembly
+            .GetManifestResourceStream($"{TestDataNamespace}.rickroll.mov");
+
+        Assert.That(movStream, Is.Not.Null, "Test MOV file should exist in embedded resources");
+
+        // Act
+        await using var gifStream = await _converter.ConvertToGif(
+            movStream,
+            frameRate: 30,
+            quality: 95,
+            maxResolution: 720
+        );
+
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.That(gifStream, Is.Not.Null);
+            Assert.That(gifStream.Length, Is.GreaterThan(0), "GIF file size should be greater than 0");
+        });
+    }
+}
---
+++ src/Tests/Mov2Gif.IntegrationTests/TestData/README.md
@@ -0,0 +1,7 @@
+This folder contains test data for the Mov2Gif.IntegrationTests project.
+The build action of these is set to "Embedded resource" so that they can be accessed by the tests
+using the `ResourceReaderUtility` class.
+
+Index:
+- `rickroll.mov` - Test MOV file captured from a Rick Astley music video.
+- `webapp.mov` - Test MOV file captured from interacting with a web page.
